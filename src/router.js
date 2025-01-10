14193e518bac4889b48ed6623cf6162ba5983495
Lucy <ziyuzhao1025@gmail.com>
2025-01-09 21:15:39 -0500

    initiate vue-router

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue'),
+        meta:{
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () => import('../views/Register.vue')
+    }
+]
+
+const router = createRouter({
+    history : createWebHistory()
+});
+export default router;
