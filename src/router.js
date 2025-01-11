cd498de414227756f44540942dfeb9e96c16520a
Lucy <ziyuzhao1025@gmail.com>
2025-01-10 23:18:13 -0500

    add styling and routing

--- src/router.js
+++ src/router.js
@@ -25,4 +25,13 @@ const router = createRouter({
     history : createWebHistory(),
     routes
 });
+router.beforeEach(async (to, from, next){
+    if(to.matched.some(record => record.meta.requiresAuth)){
+        if(!localStorage.getItem('token')){
+            return next()
+        }
+        return next('./login')
+    }
+    next()
+})
 export default router;
